062694bf6a1c55fa946c211553a01ce7b46beecd
Noir <fizpelobot@gmail.com>
2025-01-06 06:39:06 -0300

    Update efeitos.js

--- efeitos.js
+++ efeitos.js
@@ -79,3 +79,15 @@ videoElement.addEventListener('ended', () => {
     audioElement.pause();
 });
 
+// Captura o container principal e adiciona evento de clique com delegação
+document.getElementById('mainDiv').addEventListener('click', function(e) {
+    if (e.target && e.target.closest('.profile-container')) {
+        const site = e.target.closest('.profile-container').getAttribute('data-site');
+        if (site) {
+            window.location.href = site; // Redireciona para o site especificado
+        }
+    }
+});
+
+
+
