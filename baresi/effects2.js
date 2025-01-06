c815b67da26129d9083a269b3b88b149204b3d29
NOlRz <fizpelobot@gmail.com>
2025-01-06 04:15:09 -0300

    test

--- baresi/effects2.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const imgSrc = "https://lanyard.cnrad.dev/api/726952230476972162?bg=00000000&animated=true=true&hideSpotify=false&showDisplayName=true&hideActivity=true";
-    const imgElement = document.getElementById('rpc');
-    imgElement.src = imgSrc;
-});
--- members.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('mainDiv').addEventListener('click', function(e) {
-    if (e.target && e.target.closest('.profile-container')) {
-        const site = e.target.closest('.profile-container').getAttribute('data-site');
-        if (site) {
-            window.location.href = site; o
-        }
-    }
-});
