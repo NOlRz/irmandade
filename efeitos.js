523d4414f9046588ed2c076b24c5eee7d8aa675f
Noir <fizpelobot@gmail.com>
2025-01-06 05:22:09 -0300

    Update efeitos.js

--- efeitos.js
+++ efeitos.js
@@ -1,17 +1,19 @@
 function removeOverlay() {
     var overlay = document.getElementById('overlay');
     var userpage = document.getElementById('user-page');
-    var audio = document.getElementById('backgroundsong')
+    var video = document.querySelector('.background');
+    var audio = document.getElementById('backgroundsong');
 
     overlay.style.opacity = '0';
-    userpage.style.display = 'flex';
-    audio.volume = 0.3;
-    audio.play();
-
     setTimeout(function() { 
-        overlay.style.display = 'none';
-    }, 2000);
+        overlay.style.display = 'none'; // Garanta que o overlay desapareça completamente
+        userpage.style.display = 'flex'; // Mostra o conteúdo principal
+        video.style.display = 'block'; // Garanta que o vídeo esteja visível
+        video.play(); // Iniciar o vídeo
+        audio.play(); // Iniciar o áudio
+    }, 2000); // Tempo suficiente para o overlay desaparecer antes de iniciar o vídeo e o áudio
 }
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const prefix = "⠐ ";
